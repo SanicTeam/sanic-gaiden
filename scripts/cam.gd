8e28fb8637f9b2cfcf43d89e8a7550d32fa12e97
Brandon DeRosier <x@bdero.me>
2016-03-21 04:42:07 -0400

    Add camera transition support to camera scene

--- scripts/cam.gd
+++ scripts/cam.gd
@@ -3,28 +3,36 @@ extends Spatial
 
 const UP = Vector3(0, 1, 0)
 const RIGHT = Vector3(0, 0, 1)
+const FORWARD = Vector3(0, 0, -1)
 
 var cam
 
 var cam_system = null
 var cam_system_look = null
 
+var cam_system_lerp = 1
+var look_position = Vector3(0, 0, -1)
+
 func _ready():
 	cam = get_node("Camera")
 	set_process(true)
 
 func _process(delta):
-	# If there's a camera system set, snap the camera to it.
+	cam_system_lerp = min(1, cam_system_lerp + 0.5*delta)
+	
+	# If there's a camera system set, snap/ease the camera to it.
 	if cam_system != null:
-		set_global_transform(cam_system.get_global_transform())
-		set_rotation(Vector3(0, 0, 0))
+		var new_translation = get_translation().linear_interpolate(cam_system.get_global_transform().origin, cam_system_lerp)
+		set_translation(new_translation)
 	
 	if cam_system_look != null:
-		cam.look_at(cam_system_look.get_global_transform().origin, UP)
+		look_position = look_position.linear_interpolate(cam_system_look.get_global_transform().origin, cam_system_lerp)
+		cam.look_at(look_position, UP)
 
 func set_camera_system(cam_pos, look_pos):
 	cam_system = cam_pos
 	cam_system_look = look_pos
+	cam_system_lerp = 0
 
 func get_camera_system():
 	return {'pos': cam_system, 'look': cam_system_look}

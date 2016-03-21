87e556ffcf5b394b2cc21c2ab0da3a237885698b
Brandon DeRosier <x@bdero.me>
2016-03-21 04:48:36 -0400

    Removed camera transition responsibility from door

--- scripts/entities/door.gd
+++ scripts/entities/door.gd
@@ -4,9 +4,7 @@ extends Spatial
 var globals
 var animations
 
-var cam_pos
 var cam_dest
-var cam_look_pos
 var cam_look_dest
 
 var previous_cam_system
@@ -15,19 +13,10 @@ func _ready():
 	globals = get_tree().get_root().get_node("globals")
 	animations = get_node("door_model/AnimationPlayer")
 	
-	cam_pos = get_node("cam_pos")
 	cam_dest = get_node("cam_dest")
-	
-	cam_look_pos = get_node("cam_look_pos")
 	cam_look_dest = get_node("cam_look_dest")
 	
 	animations.set_current_animation("Double Opening")
-	
-	set_process(true)
-
-func _process(delta):
-	cam_pos.set_translation(cam_pos.get_translation().linear_interpolate(cam_dest.get_translation(), 2*delta))
-	cam_look_pos.set_translation(cam_look_pos.get_translation().linear_interpolate(cam_look_dest.get_translation(), 2*delta))
 
 func _on_Area_body_enter_shape( body_id, body, body_shape, area_shape ):
 	if body extends preload("res://scripts/player/sanic.gd"):
@@ -54,10 +43,7 @@ func _on_Area_body_exit_shape( body_id, body, body_shape, area_shape ):
 func take_camera():
 	previous_cam_system = globals.camera.get_camera_system()
 	
-	cam_pos.set_global_transform(previous_cam_system.pos.get_global_transform())
-	cam_look_pos.set_global_transform(previous_cam_system.look.get_global_transform())
-	
-	globals.camera.set_camera_system(cam_pos, cam_look_pos)
+	globals.camera.set_camera_system(cam_dest, cam_look_dest)
 
 func release_camera():
 	var cam_base = previous_cam_system.pos.get_parent().get_parent().get_parent().get_parent()

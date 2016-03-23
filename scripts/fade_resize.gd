e7810da150bea15d7b3ec7b20d8f34c4f3800af5
Brandon DeRosier <x@bdero.me>
2016-03-22 22:09:02 -0400

    Added fade transition for scene switching
    Closes #26

---
+++ scripts/fade_resize.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+var animations
+
+func fade_in():
+	animations.play("FadeIn")
+
+func fade_out():
+	animations.play("FadeIn", 0.5, -3, true)
+
+func _ready():
+	animations = get_node("animation")
+	set_process(true)
+
+func _process(delta):
+	var vp = get_viewport_rect().size
+	var scale = vp
+	set_scale(scale*2)
+	set_pos(scale/2)
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -6,6 +6,10 @@ var time = 0
 var rings = 0
 
 var camera
+var fade
+
+var switch_timer = null
+var next_scene_name
 
 var player = null
 var character = null
@@ -15,10 +19,17 @@ var level = null
 var paused = false
 
 func _ready():
+	var main_scene = get_tree().get_root().get_node("main")
+	
 	camera = ResourceLoader.load("res://camera.scn").instance()
-	get_tree().get_root().get_node("main").add_child(camera)
+	main_scene.add_child(camera)
+	
+	fade = ResourceLoader.load("res://fade.scn").instance()
+	main_scene.add_child(fade)
 	
 	set_scene("test_level0")
+	fade.fade_in()
+	
 	set_fixed_process(true)
 
 func _fixed_process(delta):
@@ -26,9 +37,9 @@ func _fixed_process(delta):
 		time += delta
 	
 	if(Input.is_action_pressed("key_1")):
-		set_scene("test_level0")
+		transition_scene("test_level0")
 	elif(Input.is_action_pressed("key_2")):
-		set_scene("test_level1")
+		transition_scene("test_level1")
 
 func get_time_str():
 	# minutes, seconds, and milliseconds
@@ -55,6 +66,33 @@ func resume():
 func register_player(player_object):
 	player = player_object
 	character = player.get_node("character")
+
+func transition_scene(scene):
+	if switch_timer != null:
+		return
+	
+	next_scene_name = scene
+	
+	switch_timer = Timer.new()
+	switch_timer.set_wait_time(0.8)
+	switch_timer.set_one_shot(true)
+	switch_timer.set_autostart(true)
+	switch_timer.connect("timeout", self, "_on_switch_timeout")
+	add_child(switch_timer)
+	
+	fade.fade_out()
+
+func _on_switch_timeout():
+	if next_scene_name == null:
+		print("WARNING: Scene switch timeout called, but there's no scene to switch to.")
+		return
+	set_scene(next_scene_name)
+	
+	fade.fade_in()
+	
+	switch_timer.queue_free()
+	switch_timer = null
+	next_scene_name = null
 
 func set_scene(scene):
 	# Clean up the current level if it's initialized
